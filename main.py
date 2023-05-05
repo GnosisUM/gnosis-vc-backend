2f8a35ee2ea1878760e42b5bcf261e61287c76d5
Zulfathi Hanafi <fathiimran7@gmail.com>
2023-05-06 02:19:42 +0800

    add supa

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI
-
+from database import supabase
 app = FastAPI()
 
 
@@ -17,3 +17,8 @@ fake_items_db = [{"item_name": "Foo"}, {"item_name": "Bar"}, {"item_name": "Baz"
 @app.get("/items/")
 async def read_item(skip: int = 0, limit: int = 10):
     return fake_items_db[skip : skip + limit]
+
+@app.get("/read_connection")
+async def read_connection():
+    data = supabase.table("startup_scrap").select("*").execute()
+    return data
